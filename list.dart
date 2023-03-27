fb66b2256aa8960b07de17b12212450125cb0725
mseeam <mseeam99@gmail.com>
2023-03-27 18:05:54 -0500

    initial commit

---
+++ list.dart
@@ -0,0 +1,12 @@
+
+/** (Program 2) A simple program to practice list on dart programming language
+ * @Author Mohammad Seeam */
+
+void main() {  
+   List <String> list_of_names = ["Seeam", "Sagor","Mizan","Shipa"];
+   print(list_of_names);
+   list_of_names.add("30");
+   print(list_of_names);
+}
+
+//(Program 2) ends........
